de5dc7ffa42efad380de6500c8016f421b82f8e6
iljuhs <i.pauliuk@samsung.com>
2025-03-18 16:05:10 +0100

    Add simple wezterm config

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,16 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+-- config.color_scheme = 'AdventureTime'
+
+-- Disabling character combining
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+
+-- and finally, return the configuration to wezterm
+return config
