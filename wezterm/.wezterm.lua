7638536a4871360d61d2a69859ef6949bbafb3d2
iljuhs <i.pauliuk@samsung.com>
2025-03-18 16:38:04 +0100

    Remove color scheme from wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- config.default_prog = { '/usr/bin/zsh', '-l' }
 
 -- Changing the color scheme
-config.color_scheme = 'Catppuccin Frappe'
+-- config.color_scheme = 'Catppuccin Frappe'
 
 -- Disabling character combining
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
